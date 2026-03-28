e80c36ce944f1849dacf9f62f6b2b62ec67ee85f
ZhangKeLiang0627 <111654293+ZhangKeLiang0627@users.noreply.github.com>
2026-03-28 15:11:49 +0800

    [add] 区分不同控件的最大title长度限制（#2

--- 3rdParty/HugoUI/hugo_ui_page_list.cpp
+++ 3rdParty/HugoUI/hugo_ui_page_list.cpp
@@ -94,15 +94,10 @@ void PageList::Show(Item *thisitem)
     Animation_Linear(&this->slidbar_y, &this->slidbar_y_trg, 65);
 
     // 绘制目录树和目录名
-    int16_t Item_x = static_cast<int16_t>(this->page_x);
-    int16_t Item_y;
+    int16_t item_x = static_cast<int16_t>(this->page_x);
+    int16_t item_y;
 
     // 长文本滚动参数
-    int selection_text_max_pixel = (thisitem->funcType == ItemType::Switch ||
-                                    thisitem->funcType == ItemType::ChangeValue ||
-                                    thisitem->funcType == ItemType::Checkbox) ? 96 : 120;
-    int selection_title_w = oled_get_UTF8_width(thisitem->title.c_str());
-    int sel_frame_width = (selection_title_w > selection_text_max_pixel ? selection_text_max_pixel : selection_title_w + FONT_WIDTH);
     uint8_t bar_h = ceil((float)SCREEN_HEIGHT / this->itemMax);
 
     // 遍历页面Item
@@ -114,67 +109,72 @@ void PageList::Show(Item *thisitem)
         // 不在可视区域则跳过 / 仅渲染可视区域
         if (!(item->lineId <= uiSelect - 5 || item->lineId >= uiSelect + 6))
         {
-            Item_y = FONT_HEIGHT - 1 + static_cast<int16_t>(this->page_y + item->lineId * FONT_HEIGHT + this->list_y);
+            item_y = FONT_HEIGHT - 1 + static_cast<int16_t>(this->page_y + item->lineId * FONT_HEIGHT + this->list_y);
 
             // 根据控件类型确定文本最大像素
             int textMaxPixel = (item->funcType == ItemType::Switch ||
                                 item->funcType == ItemType::ChangeValue ||
-                                item->funcType == ItemType::Checkbox) ? 107 : 120;
+                                item->funcType == ItemType::Checkbox)
+                                   ? 107
+                                   : 120;
 
             switch (item->funcType)
             {
             case ItemType::Description:
-                oled_draw_str(2 + Item_x, Item_y, "#");
-                DrawTitle(this, item, 2 + 9 + Item_x, Item_y, textMaxPixel);
+                oled_draw_str(2 + item_x, item_y, "#");
+                DrawTitle(this, item, 2 + 9 + item_x, item_y, textMaxPixel);
                 break;
 
             case ItemType::JumpPage:
-                oled_draw_str(2 + Item_x, Item_y, "+");
-                DrawTitle(this, item, 2 + 10 + Item_x, Item_y, textMaxPixel);
+                oled_draw_str(2 + item_x, item_y, "+");
+                DrawTitle(this, item, 2 + 10 + item_x, item_y, textMaxPixel);
                 break;
 
             case ItemType::Checkbox:
-                oled_draw_str(2 + Item_x, Item_y, "-");
-                DrawTitle(this, item, 2 + 9 + Item_x, Item_y, textMaxPixel);
+                oled_draw_str(2 + item_x, item_y, "-");
+                DrawTitle(this, item, 2 + 9 + item_x, item_y, textMaxPixel);
 
-                ClearControlArea(static_cast<int16_t>(SCREEN_WIDTH - FONT_WIDTH * 4 + Item_x), Item_x, Item_y);
+                // 清除长文本和控件穿模区域
+                ClearControlArea(static_cast<int16_t>(SCREEN_WIDTH - FONT_WIDTH * 4 + item_x), item_x, item_y);
 
                 // 绘制勾选标记
                 if (item->flag && *item->flag)
                 {
                     oled_set_bitmap_mode(1);
-                    oled_draw_bMP(SCREEN_WIDTH - 20 + Item_x, Item_y + 3 - FONT_HEIGHT, 10, 10, CheckBoxSelection_BMP);
+                    oled_draw_bMP(SCREEN_WIDTH - 20 + item_x, item_y + 3 - FONT_HEIGHT, 10, 10, CheckBoxSelection_BMP);
                     oled_set_bitmap_mode(0);
                 }
 
                 // 绘制勾选框
-                oled_draw_frame(SCREEN_WIDTH - 20 + Item_x, Item_y - 12 + 2, 11, 11);
+                oled_draw_frame(SCREEN_WIDTH - 20 + item_x, item_y - 12 + 2, 11, 11);
 
                 // 当前选项高亮
                 if (item->lineId == uiSelect)
                 {
                     oled_set_draw_color(2);
-                    oled_draw_box(SCREEN_WIDTH - 21 + Item_x, Item_y - 12 + 1, 13, 13);
+                    oled_draw_box(SCREEN_WIDTH - 21 + item_x, item_y - 12 + 1, 13, 13);
                     oled_set_draw_color(1);
                 }
                 break;
 
             case ItemType::Switch:
-                oled_draw_str(2 + Item_x, Item_y, "-");
-                DrawTitle(this, item, 2 + 9 + Item_x, Item_y, textMaxPixel);
+                oled_draw_str(2 + item_x, item_y, "-");
+                DrawTitle(this, item, 2 + 9 + item_x, item_y, textMaxPixel);
 
-                ClearControlArea(static_cast<int16_t>(SCREEN_WIDTH - FONT_WIDTH * 4 + Item_x), Item_x, Item_y);
+                // 清除长文本和控件穿模区域
+                ClearControlArea(static_cast<int16_t>(SCREEN_WIDTH - FONT_WIDTH * 4 + item_x), item_x, item_y);
 
                 // 显示On/Off
-                oled_draw_str(SCREEN_WIDTH - FONT_WIDTH * 3 + Item_x, Item_y,
+                oled_draw_str(SCREEN_WIDTH - FONT_WIDTH * 3 + item_x, item_y,
                               (item->flag && *item->flag) ? "On" : "Off");
                 break;
 
             case ItemType::ChangeValue:
-                oled_draw_str(2 + Item_x, Item_y, "-");
-                DrawTitle(this, item, 2 + 9 + Item_x, Item_y, textMaxPixel);
+                oled_draw_str(2 + item_x, item_y, "-");
+                DrawTitle(this, item, 2 + 9 + item_x, item_y, textMaxPixel);
 
-                ClearControlArea(static_cast<int16_t>(SCREEN_WIDTH - FONT_WIDTH * 4 + Item_x), Item_x, Item_y);
+                // 清除长文本和控件穿模区域
+                ClearControlArea(static_cast<int16_t>(SCREEN_WIDTH - FONT_WIDTH * 4 + item_x), item_x, item_y);
 
                 // 显示数值
                 if (item->param)
@@ -186,54 +186,45 @@ void PageList::Show(Item *thisitem)
                     sprintf(float_str, "%d", *item->param);
 #endif
 
-                    int16_t val_x = (*item->param < 100) ? (SCREEN_WIDTH - FONT_WIDTH * 4 + Item_x) : (SCREEN_WIDTH - FONT_WIDTH * 5 + Item_x);
-                    oled_draw_str(val_x, Item_y, float_str);
+                    int16_t val_x = (*item->param < 100) ? (SCREEN_WIDTH - FONT_WIDTH * 4 + item_x) : (SCREEN_WIDTH - FONT_WIDTH * 5 + item_x);
+                    oled_draw_str(val_x, item_y, float_str);
 
                     // 当前数值高亮
                     if (item->lineId == uiSelect && ChangeVal_flag)
                     {
                         oled_set_draw_color(2);
-                        int16_t box_x = (*item->param < 0) ? (SCREEN_WIDTH - FONT_WIDTH * 7 + Item_x) : (SCREEN_WIDTH - FONT_WIDTH * 5 + Item_x);
+                        int16_t box_x = (*item->param < 0) ? (SCREEN_WIDTH - FONT_WIDTH * 7 + item_x) : (SCREEN_WIDTH - FONT_WIDTH * 5 + item_x);
                         int16_t box_w = (*item->param < 0) ? (FONT_WIDTH * 6) : (FONT_WIDTH * 4);
-                        oled_draw_box(box_x, Item_y - 12 + 2, box_w, 11);
+                        oled_draw_box(box_x, item_y - 12 + 2, box_w, 11);
                         oled_set_draw_color(1);
                     }
                 }
                 break;
 
             default:
-                oled_draw_str(2 + Item_x, Item_y, "-");
-                DrawTitle(this, item, 2 + 9 + Item_x, Item_y, textMaxPixel);
+                oled_draw_str(2 + item_x, item_y, "-");
+                DrawTitle(this, item, 2 + 9 + item_x, item_y, textMaxPixel);
                 break;
             }
         }
-
-
     }
 
     // 清除长文本穿模滚动条的区域
     oled_set_draw_color(0);
     oled_draw_box(SCREEN_WIDTH - SCROLL_BAR_WIDTH, 0, SCROLL_BAR_WIDTH, SCREEN_WIDTH);
     oled_set_draw_color(1);
-
+    // 绘制滚动条分隔线
     for (const auto &item : this->items)
     {
         if (!item)
             continue;
 
-        // 绘制滚动条分隔线
         uint8_t bar_y = static_cast<int16_t>(this->page_y + item->lineId * bar_h);
 
         if (item->lineId % 2 == 0)
-        {
-            oled_draw_line(Item_x + SCREEN_WIDTH - 3, bar_y,
-                           Item_x + SCREEN_WIDTH - 1, bar_y);
-        }
+            oled_draw_line(item_x + SCREEN_WIDTH - 3, bar_y, item_x + SCREEN_WIDTH - 1, bar_y);
         else
-        {
-            oled_draw_line(Item_x + SCREEN_WIDTH - 3, bar_y,
-                           Item_x + SCREEN_WIDTH - 2, bar_y);
-        }
+            oled_draw_line(item_x + SCREEN_WIDTH - 3, bar_y, item_x + SCREEN_WIDTH - 2, bar_y);
     }
 
     // 绘制选择框（反色）
@@ -263,6 +254,7 @@ void PageList::Show(Item *thisitem)
         break;
 
     case State::RunPageDown:
+    {
         // 列表滚动
         if (uiSelect >= SCREEN_HEIGHT / 16)
             this->list_y_trg = -(uiSelect - 3) * FONT_HEIGHT;
@@ -276,19 +268,23 @@ void PageList::Show(Item *thisitem)
         else
             this->frame_y_trg = 3 * FONT_HEIGHT;
 
-        // if (uiSelect == 0)
-        //     this->frame_y_trg = 0;
-
-        this->frame_width_trg = sel_frame_width;
+        // 选择框宽度
+        uint16_t title_max_w = (thisitem->funcType == ItemType::Switch ||
+                                   thisitem->funcType == ItemType::ChangeValue ||
+                                   thisitem->funcType == ItemType::Checkbox)
+                                      ? 96
+                                      : 120;
+        uint16_t title_w = oled_get_UTF8_width(thisitem->title.c_str());
+        this->frame_width_trg = (title_w > title_max_w ? title_max_w : title_w + FONT_WIDTH);
 
         // 滚动条滑块
         this->slidbar_y_trg = uiSelect * ceil((float)SCREEN_HEIGHT / this->itemMax);
 
         uiState = State::None;
         break;
-
+    }
     case State::RunPageUp:
-
+    {
         // 列表滚动
         if (uiSelect >= SCREEN_HEIGHT / 16)
             this->list_y_trg = -(uiSelect - 3) * FONT_HEIGHT;
@@ -302,27 +298,34 @@ void PageList::Show(Item *thisitem)
         else
             this->frame_y_trg = 3 * FONT_HEIGHT;
 
-        this->frame_width_trg = sel_frame_width;
+        // 选择框宽度
+        uint16_t title_max_w = (thisitem->funcType == ItemType::Switch ||
+                                   thisitem->funcType == ItemType::ChangeValue ||
+                                   thisitem->funcType == ItemType::Checkbox)
+                                      ? 96
+                                      : 120;
+        uint16_t title_w = oled_get_UTF8_width(thisitem->title.c_str());
+        this->frame_width_trg = (title_w > title_max_w ? title_max_w : title_w + FONT_WIDTH);
 
         // 滚动条滑块
         this->slidbar_y_trg = uiSelect * ceil((float)SCREEN_HEIGHT / this->itemMax);
 
         uiState = State::None;
         break;
-
+    }
     case State::ReadyToJumpPage:
+    {
         // 页面位置
         this->page_x_trg = 0;
         this->page_x = 100;
 
         // 选择框位置
-        this->frame_y = SCREEN_HEIGHT * 1.5f;
+        this->frame_y = 128;
         this->frame_y_trg = (uiSelect % (SCREEN_HEIGHT / 16)) * FONT_HEIGHT;
-        this->frame_width_trg = sel_frame_width;
 
         // 滚动条位置
         this->slidbar_y = SCREEN_HEIGHT;
-        this->slidbar_y_trg = uiSelect * (SCREEN_HEIGHT / (this->itemMax + 1));
+        this->slidbar_y_trg = uiSelect * ceil((float)SCREEN_HEIGHT / this->itemMax);
 
         // 列表滚动
         if (uiSelect >= SCREEN_HEIGHT / 16)
@@ -331,17 +334,24 @@ void PageList::Show(Item *thisitem)
             this->list_y_trg = 0;
 
         // 选择框滚动
-        this->frame_y = this->frame_y_trg + FONT_HEIGHT * 1.5f;
+        this->frame_y = 128;
         if (uiSelect < SCREEN_HEIGHT / 16)
             this->frame_y_trg = uiSelect * FONT_HEIGHT;
         else
             this->frame_y_trg = 3 * FONT_HEIGHT;
 
-        if (uiSelect == 0)
-            this->list_y_trg = 0;
+        // 选择框宽度
+        uint16_t title_max_w = (thisitem->funcType == ItemType::Switch ||
+                                   thisitem->funcType == ItemType::ChangeValue ||
+                                   thisitem->funcType == ItemType::Checkbox)
+                                      ? 96
+                                      : 120;
+        uint16_t title_w = oled_get_UTF8_width(thisitem->title.c_str());
+        this->frame_width_trg = (title_w > title_max_w ? title_max_w : title_w + FONT_WIDTH);
 
         uiState = State::JumpPage;
         break;
+    }
 
     case State::JumpPage:
     {
@@ -369,10 +379,6 @@ void PageList::Show(Item *thisitem)
         break;
     }
     case State::JumpPageArrive:
-        this->frame_y_trg = (uiSelect % (SCREEN_HEIGHT / 16)) * FONT_HEIGHT;
-        this->frame_width_trg = sel_frame_width;
-        this->slidbar_y_trg = uiSelect * ceil((float)SCREEN_HEIGHT / this->itemMax);
-
         uiState = State::None;
         break;
 
